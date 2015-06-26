fdcb2f814c6ee9c27f55d2c0a7d12e6f3e042d71
linliang <lin.liang@tcl.com>
2015-06-26 20:51:41 +0800

    使用表识符替代脚本的替换，并添加设置一些模板变量

--- genresume.sh
+++ genresume.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-title=伍翀的个人简历	#网页标题
-name=伍-翀			#resume.md中的名字中有空格，要用“-”代替
-job=研发工程师		#resume.md中你填的职位
 
-pandoc -s -S resume.md -o resume.html -t html5 --self-contained --section-divs --template=resume-template.html -T $title -c css/main.css
+# 模板变量
+title="伍翀的个人简历"
+author="伍翀"
+date="2015-06-26"
+lang="zh-CN"
+
+pandoc -s -S resume.md -o resume.html -t html5 --self-contained --section-divs --template=resume-template.html -T $title -c css/main.css  \
+    --variable lang=$lang --variable author-meta=$author --variable date-meta=$date
+
 echo 'Generate resume.html success!'
-export LC_COLLATE='C'
-export LC_CTYPE='C'
-sed -i '' 's/id="'$name'"/id="name"/g'  resume.html
-sed -i '' 's/id="'$job'"/id="job"/g'  resume.html
-echo 'Done!'
